35bd14b1f27cbcaa1c39a275b7ba82cf35e29cd0
pavan <pavan49450@gmail.com>
2023-12-22 12:07:10 +0530

    Added estimated time in the products details and added a add to cart button in products page

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,9 +1,35 @@
 import { View, Text, Image, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { widthToDp, heightToDp } from "rn-responsive-screen";
 import Button from "./Button";
+import axios from "axios";
+import { Actions } from "react-native-router-flux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import baseURL from "../constants/url";
 
 export default function ProductCard({ key, product }) {
+  const [isCart, setCart] = useState(false);
+
+  const addToCart = async () => {
+    const cartId = await AsyncStorage.getItem("cart_id");
+    axios
+      .post(baseURL + "/store/carts/" + cartId + "/line-items", {
+        variant_id: product.variants[0].id,
+        quantity: 1,
+      })
+      .then(({ data }) => {
+        // alert(`Item ${product.title} added to cart`);
+        setCart(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const goToCart = () => {
+    Actions.cart();
+    setCart(false);
+  };
+
   return (
     <View style={styles.container} key={key}>
       <Image
@@ -16,11 +42,27 @@ export default function ProductCard({ key, product }) {
       <Text style={styles.category}>{product.handle}</Text>
       <View style={styles.priceContainer}>
         <Text style={styles.price}>
-          ₹{product.variants[0].prices[1]?.amount?product.variants[0].prices[1]?.amount / 100 : 50}
+          ₹
+          {product.variants[0].prices[1]?.amount
+            ? product.variants[0].prices[1]?.amount / 100
+            : 50}
         </Text>
 
-        {/* <Button
-          title="BUY"/> */}
+        {isCart ? (
+          <Button
+            onPress={goToCart}
+            title="Go to Cart"
+            // large={true}
+            style={styles.button}
+          />
+        ) : (
+          <Button
+            onPress={addToCart}
+            title="Add to Cart"
+            // large={true}
+            style={styles.button}
+          />
+        )}
       </View>
     </View>
   );
@@ -65,5 +107,8 @@ const styles = StyleSheet.create({
   price: {
     fontSize: widthToDp(4),
     fontWeight: "bold",
+  },
+  button: {
+    width: 100,
   },
 });
